f07ebb9e2e7035c8cb75735d11ed051b6c2b8916
alice <alice@tintup.com>
2015-01-30 12:53:08 -0800

    more stuff

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -14,16 +14,12 @@ class SlacksController < ApplicationController
 			player.destroy if !player.blank?
 			message = "I killed #{params[:user_name]}" if !player.blank?
 			message = "Couldn't find player" if player.blank?
+			self.rerank
 		when 'im_afk'
 			player = Player.find_by(:name => params[:user_name])
 			current_rank = player.rank
-			player.update(:status => 0, :last_rank=>player.rank, :rank=>0) if !player.blank?
-			players  = Player.where('status = 1 AND rank > 0').order('rank')
-			i = 1
-			players.each do |player|
-				player.update(:rank=>i)
-				i = i + 1
-			end
+			player.update(:status => -1, :last_rank=>player.rank, :rank=>0) if !player.blank?
+			self.rerank
 			message = "You are now inactive.  You must challenge at rank #{current_rank} in order to come back.}"
 		when 'ranking'
 			players  = Player.where('status = 1 AND rank > 0').order('rank')
@@ -33,12 +29,62 @@ class SlacksController < ApplicationController
 				message = message + "Rank #{player.rank} is #{player.name}! "
 				i = i + 1
 			end
+		when 'challenge'
+			from_user = Player.find_by(:name => params[:user_name])
+			to_user = Player.find_by(:name => body)
+
+			if from_user.blank?
+				message = "You are not a ping pong player"
+			elsif to_user.blank?
+				message = "#{body} is not a ping pong player"
+			elsif from_user.status == 0
+				message = "You cannot issue a challenge because you are already in a challenge or match"
+			elsif from_user.status == -1
+				message = "You cannot issue a challenge because you are inactive, type im_back to get back on the ranking first"
+			elsif to_user.status == 0
+				message = "#{to_user.name} is already in a challenge or match"
+			elsif to_user.status == -1
+				message = "#{to_user.name} is not active right now.  They are OOO or injured, take it easy."
+			elsif ([to_user.rank, from_user.rank].max - [to_user.rank, from_user.rank].min) > 2
+				message = "Challenge not valid, ranking difference is more than 2"
+			elsif from_user.status == 1 && to_user.status ==1
+				challenge = Challenge.create(:from_id => from_user.id, :to_id=>to_user.id, :status=> 0)
+				from_user.update(:status=>0)
+				to_user.update(:status=>0)
+				message = "Challenge to #{to_user.name} issued"
+			end
+
+		when 'accept'
+			user = Player.find_by(:name => params[:user_name])
+
+			if user.blank?
+				message = "You are not a ping pong player"
+			else
+				challenge = Challenge.where(:to_id => user.id, :status=>0).first
+				if challenge
+					from_user = Player.find(challenge.from_id)
+					challenge.update(:status=>1)
+					message = "Challenge from #{from_user.name} accepted, go play"
+				else
+					message = "No active challenges to you found"
+				end
+			end
 		else
 			message = 'Invalid command'
 		end
 
+
 		render :json => {:message=>message}, :status=>201
 	end
 
+
+	def rerank
+		players  = Player.where('status = 1 AND rank > 0').order('rank')
+		i = 1
+		players.each do |player|
+			player.update(:rank=>i)
+			i = i + 1
+		end
+	end
 
 end
