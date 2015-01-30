deb018be064aebe9b5cdbc8d6c8c7fc703045f98
alice <alice@tintup.com>
2015-01-30 12:33:22 -0800

    more stuff

--- app/controllers/slacks_controller.rb
+++ app/controllers/slacks_controller.rb
@@ -14,9 +14,25 @@ class SlacksController < ApplicationController
 			player.destroy if !player.blank?
 			message = "I killed #{params[:user_name]}" if !player.blank?
 			message = "Couldn't find player" if player.blank?
-		when 'change_status'
+		when 'im_afk'
 			player = Player.find_by(:name => params[:user_name])
-			player.update(:status => body) if body
+			current_rank = player.rank
+			player.update(:status => 0, :last_rank=>player.rank, :rank=>0) if !player.blank?
+			players  = Player.where('status = 1 AND rank > 0').order('rank')
+			i = 1
+			players.each do |player|
+				player.update(:rank=>i)
+				i = i + 1
+			end
+			message = "You are now inactive.  You must challenge at rank #{current_rank} in order to come back.}"
+		when 'ranking'
+			players  = Player.where('status = 1 AND rank > 0').order('rank')
+			i = 1
+			message = ''
+			players.each do |player|
+				message = message + "Rank #{player.rank} is #{player.name}! "
+				i = i + 1
+			end
 		else
 			message = 'Invalid command'
 		end
